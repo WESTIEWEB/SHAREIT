d5f3a89137e0029f70f16378de36969536d4b0d5
Chibuike Ilo <chibuike.ilo@decagon.dev>
2023-04-19 13:51:25 +0100

    added new mongodb connection string

--- flick_project/dist/database/index.js
+++ flick_project/dist/database/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dbConnection = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const dbConnection = async (url) => {
-    const connection = await mongoose_1.default.createConnection(url);
+    const connection = await mongoose_1.default.connect(url);
     if (connection) {
         return console.log('database connected successfully...');
     }
--- flick_project/src/database/index.ts
+++ flick_project/src/database/index.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const dbConnection = async(url: string) => {
-   const connection = await mongoose.createConnection(url);
+   const connection = await mongoose.connect(url);
    if(connection) {
     return console.log('database connected successfully...')
    }
